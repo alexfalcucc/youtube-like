92e89c003625c41fb3b4a3b9dee078b8a47fa27c
Alexsander Falcucci <alex.falcucci@gmail.com>
2017-11-07 00:01:48 -0200

    Updated

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="text-center">
+      © 2017 <a href="https://github.com/alexfalcucc" target="_blank">Alexsander Falcucci</a>
+    </div>
+  </footer>
+);
+
+export default Footer;
